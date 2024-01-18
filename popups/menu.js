c2fc5c67e7fba9f2991500730294edc2d2f08c71
nguyenjessev <67484662+nguyenjessev@users.noreply.github.com>
2024-01-18 14:06:53 -0500

    Show popup on icon click and show menu if in editor

---
+++ popups/menu.js
@@ -0,0 +1,13 @@
+(() => {
+  browser.tabs
+    .query({
+      active: true,
+      currentWindow: true,
+    })
+    .then((data) => {
+      if (!data[0].url.includes('email_template_studio')) {
+        document.querySelector('.popup-content').classList.add('hidden');
+        document.querySelector('.error-content').classList.remove('hidden');
+      }
+    });
+})();
