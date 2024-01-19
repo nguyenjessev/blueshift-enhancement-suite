a07c939d670eecc0a6d77cef280ab58a5b8d227d
nguyenjessev <67484662+nguyenjessev@users.noreply.github.com>
2024-01-19 13:21:15 -0500

    Fetch recommendation block data

--- content_scripts/refreshData.js
+++ content_scripts/refreshData.js
@@ -9,14 +9,28 @@
     const regex = /email_template_studio\/(.+)\/edit/;
     const templateId = window.location.href.match(regex)[1];
 
+    const recommendationDataResponse = await fetch(
+      `https://app.getblueshift.com/api/v1/email_templates/${templateId}.json`,
+      {
+        method: 'GET',
+      }
+    );
+    const recommendationData = await recommendationDataResponse.json();
+
+    const recommendationId =
+      recommendationData.template_properties[0].account_algorithm_id;
+
     const templateDataResponse = await fetch(
       'https://app.getblueshift.com/api/v1/personalizations/template.json',
       {
+        body: `{"template_properties":{"account_algorithm_id":${recommendationId},"algorithm":"${recommendationId}"}}`,
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
         method: 'POST',
-        body: {},
       }
     );
-
     const templateData = await templateDataResponse.json();
 
     browser.runtime.sendMessage({
