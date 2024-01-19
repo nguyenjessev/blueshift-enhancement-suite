b53ef8b690d58b999bbfe8ed7f65f8637f07058f
nguyenjessev <67484662+nguyenjessev@users.noreply.github.com>
2024-01-19 13:49:54 -0500

    Fetch data for currently previewed user

--- content_scripts/refreshData.js
+++ content_scripts/refreshData.js
@@ -6,8 +6,8 @@
   window.BESRefreshDataHasRun = true;
 
   const refreshData = async () => {
-    const regex = /email_template_studio\/(.+)\/edit/;
-    const templateId = window.location.href.match(regex)[1];
+    const templateRegex = /email_template_studio\/(.+)\/edit/;
+    const templateId = window.location.href.match(templateRegex)[1];
 
     const recommendationDataResponse = await fetch(
       `https://app.getblueshift.com/api/v1/email_templates/${templateId}.json`,
@@ -16,14 +16,18 @@
       }
     );
     const recommendationData = await recommendationDataResponse.json();
-
     const recommendationId =
       recommendationData.template_properties[0].account_algorithm_id;
+
+    const userRegex = /customer\/(.+)\/show/;
+    const userId = document
+      .querySelector('.customer-link-out')
+      .href.match(userRegex)[1];
 
     const templateDataResponse = await fetch(
       'https://app.getblueshift.com/api/v1/personalizations/template.json',
       {
-        body: `{"template_properties":{"account_algorithm_id":${recommendationId},"algorithm":"${recommendationId}"}}`,
+        body: `{"template_properties":{"account_algorithm_id":${recommendationId},"algorithm":"${recommendationId}"},"uuid":"${userId}"}`,
         credentials: 'include',
         headers: {
           'Content-Type': 'application/json;charset=utf-8',
