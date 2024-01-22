db3428355288ff27523d2c02d55ead8bab09e145
nguyenjessev <67484662+nguyenjessev@users.noreply.github.com>
2024-01-22 13:26:43 -0500

    Make calculator button persist

--- content_scripts/calculateFilesize.js
+++ content_scripts/calculateFilesize.js
@@ -1,35 +1,55 @@
-const calculatorContainer = document.createElement('li');
-calculatorContainer.classList.add('calculate-filesize-btn');
-calculatorContainer.classList.add('tab-item');
+const calculatorMain = () => {
+  const calculatorContainer = document.createElement('li');
+  calculatorContainer.classList.add('calculate-filesize-btn');
+  calculatorContainer.classList.add('tab-item');
 
-const calculatorContents = document.createElement('div');
-calculatorContents.classList.add('calculator-contents');
-calculatorContents.append('Calculate Filesize:');
+  const calculatorContents = document.createElement('div');
+  calculatorContents.classList.add('calculator-contents');
+  calculatorContents.append('Calculate Filesize:');
 
-const size = document.createElement('span');
-size.classList.add('size');
-size.textContent = 'N/A';
+  const size = document.createElement('span');
+  size.classList.add('size');
+  size.textContent = 'N/A';
 
-calculatorContents.appendChild(size);
-calculatorContainer.appendChild(calculatorContents);
+  calculatorContents.appendChild(size);
+  calculatorContainer.appendChild(calculatorContents);
 
-const toolbar = document.querySelector('.bsft-tab-theme-box > ul');
-toolbar.appendChild(calculatorContainer);
+  const toolbar = document.querySelector('.bsft-tab-theme-box > ul');
+  toolbar.appendChild(calculatorContainer);
 
-calculatorContainer.addEventListener('click', async () => {
-  const templateRegex = /email_template_studio\/(.+)\/edit/;
-  const templateId = window.location.href.match(templateRegex)[1];
+  calculatorContainer.addEventListener('click', async () => {
+    const templateRegex = /email_template_studio\/(.+)\/edit/;
+    const templateId = window.location.href.match(templateRegex)[1];
 
-  const templateDataResponse = await fetch(
-    `https://app.getblueshift.com/api/v1/email_templates/${templateId}.json`,
-    {
-      method: 'GET',
+    const templateDataResponse = await fetch(
+      `https://app.getblueshift.com/api/v1/email_templates/${templateId}.json`,
+      {
+        method: 'GET',
+      }
+    );
+    const templateData = await templateDataResponse.json();
+    const templateHTML = templateData.resource.content;
+    const templateSize = Math.round(new Blob([templateHTML]).size / 100) / 10;
+
+    const size = document.querySelector('.size');
+    size.textContent = `~${templateSize}KB`;
+  });
+};
+
+const observeToolbar = (selector) => {
+  const observer = new MutationObserver((mutations) => {
+    if (
+      document.querySelector(selector) &&
+      !document.querySelector('.calculate-filesize-btn')
+    ) {
+      calculatorMain();
     }
-  );
-  const templateData = await templateDataResponse.json();
-  const templateHTML = templateData.resource.content;
-  const templateSize = Math.round(new Blob([templateHTML]).size / 100) / 10;
+  });
 
-  const size = document.querySelector('.size');
-  size.textContent = `~${templateSize}KB`;
-});
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+  });
+};
+
+observeToolbar('.TemplateStudio-editor .bsft-tab-theme-box > ul');
